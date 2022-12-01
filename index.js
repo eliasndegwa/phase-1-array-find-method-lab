3785473b0382b8b729ab83926d6bc4dfd4f8e2fb
eliasndegwa <eliasndegwamwai@gmail.com>
2022-12-01 09:17:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    
+      let find=record.find(record=>record.result==="W")
+      if (find) {
+        return find.year
+      } else {
+        return undefined
+      }
+}
